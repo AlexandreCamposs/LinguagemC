2609aaddb6176ed5a5b33605ded5314b38c8e048
AlexandreCamposs <89165912+AlexandreCamposs@users.noreply.github.com>
2021-09-25 19:02:29 -0300

    att

--- ex03.c
+++ ex03.c
@@ -3,4 +3,10 @@ Tipos básicos
 Tipos de enumeração
 Tipos void
 Tipos derivados (Structs, arrays, ponteiros,unions,etc)*/
-
+---------------
+int a, b, c;
+char letra;
+float valor, aumento;
+double patrimonio;
+int x = 0;
+float salario, juros = 0.08;
